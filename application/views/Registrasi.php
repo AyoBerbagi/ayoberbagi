d6d3ea1c428efe39be9d2d148c3c9aa2bff3a7be
Megasinagaa17 <31728876+Megasinagaa17@users.noreply.github.com>
2018-05-07 13:58:55 +0700

    Add files via upload

--- application/views/Registrasi.php
+++ application/views/Registrasi.php
@@ -21,65 +21,59 @@
 <body class="template-page sidebar-collapse">
     <!-- Navbar -->
     <nav class="navbar navbar-expand-lg bg-primary fixed-top" color-on-scroll="400">
-      <div class="container">
-          <div class="navbar-translate">
-              <a class="navbar-brand" href="<?=base_url()?>" rel="tooltip">
-                <img src="<?=base_url()?>assets/img/ayoberbagi.png" alt="" style="height: 100px;width: 150px;margin-top: -40px;margin-bottom: -50px;">
-              </a>
-          </div>
-          <div class="collapse navbar-collapse justify-content-end" id="navigation" data-nav-image="./assets/img/blurred-image-1.jpg">
-              <ul class="navbar-nav">
-                  <li class="nav-item">
-                      <a class="nav-link" href="<?=base_url()?>" >
-                          <i class=""></i>
-                          <p>Home</p>
-                      </a>
-                  </li>
-                  <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                      <p>
-                        Bentuk Donasi
-                      </p>
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                      <a class="dropdown-item" href="<?=base_url()?>berbagiDana">Donasi </a>
-                      <a class="dropdown-item" href="<?=base_url()?>BerbagiBarang">Berbagi Barang</a>
-                      <a class="dropdown-item" href="<?=base_url()?>timSukarelawan">Aksi Sosial</a>
-                      <a class="dropdown-item" href="<?=base_url()?>Kampanye">Kampanye Baru</a>
-                    </div>
-                  </li>
-                   <!-- <li class="nav-item">
-                    <div class="">
-                      <a class="nav-link" href="<?=base_url()?>index.php/LogOut"><i class="now-ui-icons users_single-02" aria-hidden="true"></i>Log Out</a>
-                    </div>
-                  </li> -->
-                  <?php if($this->session->has_userdata('login')){
-                    echo "<li class='nav-item dropdown'>
-                      <a class='nav-link dropdown-toggle' href='#' id='navbarDropdownMenuLink' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>
-                        <p>";
-                    echo $_SESSION['nama'];
-                    echo "</p>
-                      </a>
-                      <div class='dropdown-menu' aria-labelledby='navbarDropdownMenuLink'>
-                        <a class='dropdown-item' href='".base_url()."Profil'>Profil </a>
-                        <a class='dropdown-item' href='".base_url()."LogOut'>Log Out</a>
-                      </div>
-                      </li>";
-                  }else{
-                    echo "<li class='nav-item'>
-                    <a class='nav-link' href='".base_url()."LogIn'><i class='now-ui-icons users_single-02' aria-hidden='true'></i>Log In</a>
-                    </li>";
-                  } ?>
-                   </ul>
-                </div>
-              </div>
-          </nav>
+        <div class="container">
+            <div class="navbar-translate">
+                <a class="navbar-brand" href="http://ayoberbagii.000webhostapp.com" rel="tooltip" >
+                    Ayo Berbagi
+                </a>
+                <button class="navbar-toggler navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-bar bar1"></span>
+                    <span class="navbar-toggler-bar bar2"></span>
+                    <span class="navbar-toggler-bar bar3"></span>
+                </button>
+            </div>
+            <div class="collapse navbar-collapse justify-content-end" id="navigation" data-nav-image="../assets/img/blurred-image-1.jpg">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?=base_url()?>">
+                            <p>Home</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?=base_url()?>index.php/register">
+                            <p>Registrasi</p>
+                        </a>
+                    </li>
+                    <!-- <li class="nav-item">
+                        <a class="nav-link" rel="tooltip" title="Follow us on Twitter" data-placement="bottom" href="https://twitter.com/CreativeTim" target="_blank">
+                            <i class="fa fa-twitter"></i>
+                            <p class="d-lg-none d-xl-none">Twitter</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" rel="tooltip" title="Like us on Facebook" data-placement="bottom" href="https://www.facebook.com/CreativeTim" target="_blank">
+                            <i class="fa fa-facebook-square"></i>
+                            <p class="d-lg-none d-xl-none">Facebook</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" rel="tooltip" title="Follow us on Instagram" data-placement="bottom" href="https://www.instagram.com/CreativeTimOfficial" target="_blank">
+                            <i class="fa fa-instagram"></i>
+                        </a>
+                    </li> -->
+                </ul>
+            </div>
+        </div>
+    </nav>
     <!-- End Navbar -->
 
     </div>
     <div class="wrapper">
+
         <div class="page-header">
-            <div class="page-header-image" data-parallax="true" style="background-image:url('<?=base_url()?>/assets/img/love2.jpg');">
+            
+
+            <div class="page-header-image" data-parallax="true" style="background-image:url('<?=base_url()?>/assets/img/Background12.jpg');">
             </div>
 
 <!-- form registrasi -->
@@ -167,38 +161,37 @@
                 </div>
             </div>
         </div>
+
+
+
+
+
         </div>
+        <div class="section">
         </div>
-        <footer class="footer" data-background-color="violet">
+        <footer class="footer">
             <div class="container">
+
                 <nav>
                     <ul>
-                    	<div class= "row">
-                	<div class= "col-md-3">
-                    <div id="buttons">
-                        <p class="category">Ayo Berbagi
-                        adalah website untuk berdonasi dana , barang dan penghubung untuk melakukan aksi sosial secara online.  </p>
-                        </div>
-                        </div>
-                        <div class= "col-md-3"><button class="btn">Bentuk Donasi</button>
-                        <p class="category">Bebagi Donasi </p>
-                        <p class="category">Bebagi Barang </p>
-                        <p class="category">Aksi Sosial </p>
-                        <p class="category">Pengabdian Masyarakat </p>
-                        </div>
-                        <div class="col-md-3 col-lg-3"><button class="btn btn-primary">Pelajari Lebih Lanjut</button>
-                        <p class="category">Tentang Kami </p>
-                        <p class="category"><a href="<?=base_url()?>FAQ">F.A.Q</a></p>
-                        <p class="category">Bantuan </p>
-                        <p class="category">Tim Sukarelawan </p>
-                        </div>
-                        <div class="col-md-3"><button class="btn btn-info">Kontak Bantuan</button>
-                        <p class="category">082304480707 </p>
-                        <p class="category">ayo.berbagi@gmail.com </p>
-                        <p class="category">Institut Teknologi Sumatera </p>
-                        </div>
-                        </div>
-
+                        <li>
+                            <a href="https://www.creative-tim.com">
+                                Creative Tim
+                            </a>
+                        </li>
+                        <li>
+                            <a href="http://presentation.creative-tim.com">
+                                About Us
+                            </a>
+                        </li>
+                        <li>
+                            <a href="http://blog.creative-tim.com">
+                                Blog
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://github.com/creativetimofficial/now-ui-kit/blob/master/LICENSE.md">
+                                MIT License
                             </a>
                         </li>
                     </ul>
@@ -213,34 +206,19 @@
                 </div>
             </div>
         </footer>
-
+    </div>
 </body>
 <!--   Core JS Files   -->
-<script src="<?=base_url()?>assets/js/core/jquery.3.2.1.min.js" type="text/javascript"></script>
-<script src="<?=base_url()?>assets/js/core/popper.min.js" type="text/javascript"></script>
-<script src="<?=base_url()?>assets/js/core/bootstrap.min.js" type="text/javascript"></script>
+<script src="../assets/js/core/jquery.3.2.1.min.js" type="text/javascript"></script>
+<script src="../assets/js/core/popper.min.js" type="text/javascript"></script>
+<script src="../assets/js/core/bootstrap.min.js" type="text/javascript"></script>
 <!--  Plugin for Switches, full documentation here: http://www.jque.re/plugins/version3/bootstrap.switch/ -->
-<script src="<?=base_url()?>assets/js/plugins/bootstrap-switch.js"></script>
+<script src="../assets/js/plugins/bootstrap-switch.js"></script>
 <!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
-<script src="<?=base_url()?>assets/js/plugins/nouislider.min.js" type="text/javascript"></script>
+<script src="../assets/js/plugins/nouislider.min.js" type="text/javascript"></script>
 <!--  Plugin for the DatePicker, full documentation here: https://github.com/uxsolutions/bootstrap-datepicker -->
-<script src="<?=base_url()?>assets/js/plugins/bootstrap-datepicker.js" type="text/javascript"></script>
+<script src="../assets/js/plugins/bootstrap-datepicker.js" type="text/javascript"></script>
 <!-- Control Center for Now Ui Kit: parallax effects, scripts for the example pages etc -->
-<script src="<?=base_url()?>assets/js/now-ui-kit.js?v=1.1.0" type="text/javascript"></script>
-<script type="text/javascript">
-    $(document).ready(function() {
-        // the body of this function is in assets/js/now-ui-kit.js
-        nowuiKit.initSliders();
-    });
-
-    function scrollToDownload() {
-
-        if ($('.section-download').length != 0) {
-            $("html, body").animate({
-                scrollTop: $('.section-download').offset().top
-            }, 1000);
-        }
-    }
-</script>
+<script src="../assets/js/now-ui-kit.js?v=1.1.0" type="text/javascript"></script>
 
 </html>
